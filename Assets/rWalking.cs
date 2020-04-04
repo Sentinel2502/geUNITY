b39e9d8a3ad51aeed3e95206ab8b078f949c89da
Sentinel <wildpython@yandex.ru>
2020-04-04 19:38:26 +0300

    добавлена анимация ходьбы для персонажа Алиса

--- Assets/rWalking.cs
+++ Assets/rWalking.cs
@@ -1,62 +1,82 @@
-/*using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+//using System.Collections;
+using System;
 
 public class rWalking : MonoBehaviour
 {
-    public float speed = 150f;
-    public Rigidbody rb; 
-    public Vector3 movement;
+    Animator animator;
 
-    void Start ()
-    {
-        rb = this.GetComponent<Rigidbody>();
-    }
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.D))
-            movement = new Vector3(0, 0, -1);
-            //rb.MovePosition(transform.position + (speed * Time.deltaTime * new Vector3(0, 0, -1)));
-        if (Input.GetKey(KeyCode.A))
-            movement = new Vector3(0, 0, 1);
-            //rb.MovePosition(transform.position + (speed * Time.deltaTime * new Vector3(0, 0, 1)));
-        if (Input.GetKey(KeyCode.W))
-            movement = new Vector3(1, 0, 0);
-            //rb.MovePosition(transform.position + (speed * Time.deltaTime * new Vector3(1, 0, 0)));
-        if (Input.GetKey(KeyCode.S))
-            movement = new Vector3(-1, 0, 0);
-            //rb.MovePosition(transform.position + (speed * Time.deltaTime * new Vector3(-1, 0, 0)));
-    }
-
-    void FixedUpdate()
-    {
-        MoveCharacter(movement);
-        movement = new Vector3(0, 0, 0);
-    }
-
-    void MoveCharacter(Vector3 direction)
-    {
-        rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
-    }
-}*/
-using UnityEngine;
-
-public class rWalking : MonoBehaviour
-{
     public float moveSpeed = 100f;
-    // Use this for initialization
+
+    float vertical = 0;
+    float horizontal = 0;
+
+    //animation states - the values in the animator conditions
+    const int CHAR_STAND = 0;
+    const int CHAR_RIGHT = 1;
+    const int CHAR_LEFT = 2;
+    const int CHAR_FOR = 3;
+    const int CHAR_BACK = 4;
+
+    int _currentAnimationState = CHAR_STAND;
+
     void Start()
     {
-
+        //define the animator attached to the player
+        animator = this.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        vertical  = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
         //Moves Forward and back along z axis //Up/Down
-        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * vertical * moveSpeed);
         //Moves Left and right along x Axis //Left/Right
-        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontal * moveSpeed);
+
+        if (Math.Abs(vertical) < 0.7 && Math.Abs(horizontal) < 0.7)
+            changeState(CHAR_STAND);
+        else if (vertical > 0)
+            changeState(CHAR_BACK);
+        else if (vertical < 0)
+            changeState(CHAR_FOR);
+        else if (horizontal > 0)
+            changeState(CHAR_RIGHT);
+        else if (horizontal < 0)
+            changeState(CHAR_LEFT);
+    }
+
+    void changeState(int state){
+ 
+        if (_currentAnimationState == state)
+        return;
+ 
+        switch (state) {
+ 
+        case CHAR_RIGHT:
+            animator.SetInteger ("state", CHAR_RIGHT);
+            break;
+ 
+        case CHAR_LEFT:
+            animator.SetInteger ("state", CHAR_LEFT);
+            break;
+ 
+        case CHAR_FOR:
+            animator.SetInteger ("state", CHAR_FOR);
+            break;
+ 
+        case CHAR_BACK:
+            animator.SetInteger ("state", CHAR_BACK);
+            break;
+ 
+        case CHAR_STAND:
+            animator.SetInteger ("state", CHAR_STAND);
+            break;
+ 
+        }
+ 
+        _currentAnimationState = state;
     }
 } 
